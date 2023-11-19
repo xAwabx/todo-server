fd1c752939de12b0fbbb290f62f29892d499f7c8
Awab <awabsaghir.8@gmail.com>
2023-11-19 21:10:59 +0500

    Add database

--- index.js
+++ index.js
@@ -1,78 +1,64 @@
 const express = require('express');
 var bodyParser = require('body-parser')
 var cors = require('cors')
+const mongoose = require("mongoose")
 
 const app = express();
 
+app.use(express.json())
 app.use(cors()) 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-let todoListData = [
-  {
-    id: 53, 
-    text: "do your dishes",
-    isChecked: false,
-  },
-  {
-    id: 63, 
-    text: "go to the gym",
-    isChecked: false
-  },
-  {
-    id: 82, 
-    text: "do your chores",
-    isChecked: false
-  },
-  {
-    id: 92, 
-    text: "go to college",
-    isChecked: false
-  }
-]
+// Connect to DB
+const DB_PASSWORD = 'Awabsaghir4231';
+const DB_CONNECT_STATEMENT = `mongodb+srv://awabsaghir:${DB_PASSWORD}@todolistcluster.u0tkcnw.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_CONNECT_STATEMENT);
+
+const todoSchema = mongoose.Schema({
+  text: { type: String, requrired: true },
+  isChecked: { type: Boolean, default: false }
+})
+
+const Todo = mongoose.model('Todo', todoSchema);
 
 // GET - todo data
 
 app.get('/todos', (req, res) => {
-  res.json(todoListData);
+  Todo.find().then((todosData)=> res.json(todosData))
 });
 
 // POST - todo
 // required json - {Text-string}
 
-app.post("/todos/create", (req, res) => {
-  console.log(req.body)
-  let newTodo = {
-    id: Math.floor(Math.random() * 100001),
-    text: req.body.text, 
-    isChecked: false
-  } 
-  todoListData.push(newTodo)
-  res.json(todoListData)
+app.post("/todos/create", async (req, res) => {
+  await Todo.create({text: req.body.text})
+  Todo.find().then((todosData)=> res.json(todosData))
 })
 
 // PATCH - updating "isChecked" if checked is clicked
 // required json - {id-int}
 
-app.patch("/todos/update", (req, res)=> {
+app.patch("/todos/update", async (req, res)=> {
   let id = req.body.id
   console.log(id, req.body)
-  todoListData.forEach((todo, index) => {
-    todo.id == id && (todo.isChecked = !todo.isChecked)
-  });
-  res.json(todoListData)
-  console.log(todoListData)
-
+  try {
+    doc = await Todo.findById({_id : id}).exec()
+    doc.isChecked = !doc.isChecked
+    await doc.save()
+    Todo.find().then((todosData)=> res.json(todosData))
+  } catch(error) {
+    console.log("ERROR: ", error)
+  }
 })
 
 // DELETE - deleteing todo with given id
 // required json - {id-int}
 
-app.delete("/todos/delete", (req, res) => {
+app.post("/todos/delete", async(req, res) => {
   id = req.body.id
-
-  todoListData.forEach((todo, index) => todo.id == id && todoListData.splice(index, 1) )
-  res.json(todoListData)
-
+  await Todo.findByIdAndDelete(id)
+  Todo.find().then((todosData)=> res.json(todosData))
 })
 
 app.listen(4000, () => console.log('Example app is listening on port 4000.'));
